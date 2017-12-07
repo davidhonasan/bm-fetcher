eda5e76b7ab4ba43f7eee854c6e923cacfb07826
davidhonasan <david_honasan@live.com>
2017-12-07 21:16:33 +0700

    First Commit

--- bm/settings.py
+++ bm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pob&g+fptkbx*kk#5tr3-@v7tay=re44rk901lq=$-aug@0bow'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Import private things
+from bm.settings_private import *
--- schedule/views.py
+++ schedule/views.py
@@ -8,8 +8,8 @@ def schedule(request):
     context = {
         # Get BM Class Schedule JSON
         'schedule': requests.get('https://binusmaya.binus.ac.id/services/ci/index.php/student/class_schedule/classScheduleGetStudentClassSchedule',
-                                   header={'Referer': 'https://binusmaya.binus.ac.id/newStudent/'},
-                                   cookie=request.session.get('session_cookie')).json(),
+                                 headers={'Referer': 'https://binusmaya.binus.ac.id/newStudent/index.html'},
+                                 cookies=request.session.get('session_cookie')).json(),
         'name': request.session.get('name', 'Name').title(),
         'major': request.session.get('major', 'Major'),
         'student_id': request.session.get('student_id', 'Student ID'),
